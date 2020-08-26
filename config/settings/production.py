15aa810c0db35cbe6b59faedde08c5f23fc9d1e5
Denis Kolosov <denis.kolosov@gmail.com>
2020-08-26 23:04:00 +0300

    Use s3 for static

--- config/settings/production.py
+++ config/settings/production.py
@@ -89,7 +89,7 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "afi_backend.utils.storages.MediaRootS3Boto3Storage"
