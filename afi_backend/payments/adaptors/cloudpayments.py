e12dd1a3c12b0dea4d17bc9ca5b921783d5f9d5e
Denis Kolosov <denis.kolosov@gmail.com>
2020-09-03 16:08:23 +0300

    Cloudpayments adaptor

--- afi_backend/payments/adaptors/cloudpayments.py
+++ afi_backend/payments/adaptors/cloudpayments.py
@@ -1,4 +1,17 @@
 
 
-class CloudpaymentsAdaptor:
-    pass
+from .base import BasePaymentAdaptor
+
+
+class CloudpaymentsAdaptor(BasePaymentAdaptor):
+    """
+    """
+    def __init__(self):
+        """
+        Configure cloudpayments here
+        """
+        pass
+
+
+    def charge(self, value:str, currency:str, description:str, internal_payment_id:uuid.UUID):
+        pass
