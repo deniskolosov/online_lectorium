715b47635a51f86e99c69e28a1af3e19ce7f6389
Denis Kolosov <denis.kolosov@gmail.com>
2020-09-04 17:12:11 +0300

    Add url for Cloudpayments webhook

--- config/api_router.py
+++ config/api_router.py
@@ -1,9 +1,8 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from afi_backend.payments.urls import PaymentCreateView, YandexWebhook
 from afi_backend.users.api.views import UserViewSet
-from afi_backend.payments.api.views import PaymentMethodViewset
+from afi_backend.payments.api.views import PaymentMethodViewset, PaymentCreateView, YandexWebhook, CloudpaymentsWebhook
 from django.urls import path
 
 
@@ -18,6 +17,7 @@ router.register("payment_methods", PaymentMethodViewset)
 urlpatterns = [
     path("payments/", view=PaymentCreateView.as_view(), name="payment-create-view"),
     path("checkout-wh/", YandexWebhook.as_view(), name="yandex-checkout-webhook"),
+    path("cloudpayments-wh/", CloudpaymentsWebhook.as_view(), name="cloudpayments-webhook"),
  ]
 
 app_name = "api"
