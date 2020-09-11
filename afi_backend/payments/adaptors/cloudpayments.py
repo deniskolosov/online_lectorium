f97214d089bb7c1ecbd17a1bb2dc06b738112224
dkol <denis.kolosov@gmail.com>
2020-09-11 18:27:48 +0300

    Convert uuid to string

--- afi_backend/payments/adaptors/cloudpayments.py
+++ afi_backend/payments/adaptors/cloudpayments.py
@@ -1,12 +1,14 @@
+import uuid
+
 from django.conf import settings
 from django.urls import reverse
 
-import uuid
-from .base import BasePaymentAdaptor
+from afi_backend.payments.adaptors.base import BasePaymentAdaptor
 
 
 class CloudpaymentsAdaptor(BasePaymentAdaptor):
     """
+    Adapter for CloudPayments integration
     """
     def __init__(self):
         """
@@ -14,14 +16,16 @@ class CloudpaymentsAdaptor(BasePaymentAdaptor):
         """
         pass
 
-
-    def charge(self, value:str, currency:str, description:str, internal_payment_id:uuid.UUID):
+    def charge(self, value: str, currency: str, description: str,
+               internal_payment_id: uuid.UUID):
         """
         Return link to template view containing Cloudpayments form with all necessary data
         """
-        return settings.SITE_URL + reverse('payments:cloudpayments-payment-form', kwargs={
-            'amount':value,
-            'description': description,
-            'currency': currency,
-            'payment_id': internal_payment_id,
-        })
+        return settings.SITE_URL + reverse(
+            'payments:cloudpayments-payment-form',
+            kwargs={
+                'amount': value,
+                'description': description,
+                'currency': currency,
+                'payment_id': str(internal_payment_id),
+            })
