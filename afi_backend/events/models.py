88aa5b6181b7dd094061dc6af7ac208bd3a0f0d5
dkol <denis.kolosov@gmail.com>
2020-09-14 14:27:21 +0300

    Add extra fields to events models

--- afi_backend/events/models.py
+++ afi_backend/events/models.py
@@ -1,11 +1,13 @@
 import time
 
-from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from djmoney.models.fields import MoneyField
 
 from afi_backend.tickets.models import Ticket
+from django.contrib.contenttypes.fields import (GenericForeignKey,
+    GenericRelation)
+from django.contrib.contenttypes.models import ContentType
+from djmoney.models.fields import MoneyField
+from afi_backend.users.models import User
 
 
 class Event(models.Model):
@@ -25,10 +27,22 @@ class Lecturer(models.Model):
     userpic = models.ImageField(upload_to='lecturer_userpics/',
                                 blank=True,
                                 null=True)
+    bio = models.TextField(null=True, blank=True)
+
+
+class LectureCategory(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+
+
+class LectureRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
 
 
 class LectureBase(models.Model):
     name = models.CharField(max_length=256)
+    picture = models.ImageField(upload_to='lecture_pictures', null=True, blank=True)
     description = models.TextField()
     lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
     lecture_summary_file = models.FileField(upload_to='lecture_summaries/',
@@ -39,6 +53,8 @@ class LectureBase(models.Model):
                        decimal_places=2,
                        null=True,
                        default_currency='RUB')
+    category = models.ForeignKey(LectureCategory, on_delete=models.CASCADE)
+    rating = models.ForeignKey(LectureRating, on_delete=models.CASCADE, null=True, blank=True)
 
 
 class OfflineLecture(LectureBase):
