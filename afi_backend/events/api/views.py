dab9d0c448c997620b8c2a5c4b656ec747445030
dkol <denis.kolosov@gmail.com>
2020-09-17 18:24:58 +0300

    Add filtering and search

--- afi_backend/events/api/views.py
+++ afi_backend/events/api/views.py
@@ -1,11 +1,16 @@
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets
 from rest_framework import filters as drf_filters
-from rest_framework_json_api import relations, serializers, filters, django_filters
+from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_json_api import django_filters, filters, relations, serializers
 
-from ..models import Event, OfflineLecture, Lecturer
-from .serializers import EventSerializer, OfflineLectureSerializer, LecturerSerializer
+from ..models import Event, LectureCategory, Lecturer, OfflineLecture
+from .serializers import (
+    EventSerializer,
+    LectureCategorySerializer,
+    LecturerSerializer,
+    OfflineLectureSerializer,
+)
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -30,6 +35,26 @@ class OfflineLectureViewset(viewsets.ModelViewSet):
             'exact',
             'gt',
             'lt',
+        ),
+        'category__name': (
+            'icontains',
+        ),
+    }
+    search_fields = ['name']
+
+
+class LectureCategoriesViewSet(viewsets.ModelViewSet):
+    queryset = LectureCategory.objects.all()
+    serializer_class = LectureCategorySerializer
+    filter_backends = (
+        filters.QueryParameterValidationFilter,
+        django_filters.DjangoFilterBackend,
+        drf_filters.SearchFilter,
+    )
+    permission_classes = [IsAuthenticated]
+    filterset_fields = {
+        'name': (
+            'icontains',
         )
     }
     search_fields = ['name']
@@ -39,3 +64,14 @@ class LecturersViewset(viewsets.ModelViewSet):
     queryset = Lecturer.objects.all()
     serializer_class = LecturerSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = (
+        filters.QueryParameterValidationFilter,
+        django_filters.DjangoFilterBackend,
+        drf_filters.SearchFilter,
+    )
+    filterset_fields = {
+        'name': (
+            'icontains',
+        )
+    }
+    search_fields = ['name']
--- config/api_router.py
+++ config/api_router.py
@@ -1,12 +1,22 @@
 from django.conf import settings
+from django.urls import path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from afi_backend.users.api.views import UserViewSet
+from afi_backend.events.api.views import (
+    EventViewSet,
+    LectureCategoriesViewSet,
+    LecturersViewset,
+    OfflineLectureViewset,
+    LectureCategoriesViewSet,
+)
+from afi_backend.payments.api.views import (
+    CloudpaymentsWebhook,
+    PaymentCreateView,
+    PaymentMethodViewset,
+    YandexWebhook,
+)
 from afi_backend.tickets.api.views import TicketViewSet
-from afi_backend.events.api.views import EventViewSet, OfflineLectureViewset, LecturersViewset
-from afi_backend.payments.api.views import PaymentMethodViewset, PaymentCreateView, YandexWebhook, CloudpaymentsWebhook
-from django.urls import path
-
+from afi_backend.users.api.views import UserViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -14,17 +24,30 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
-router.register("payment_methods", PaymentMethodViewset, basename='payment-methods')
+router.register("payment_methods",
+                PaymentMethodViewset,
+                basename='payment-methods')
 router.register("tickets", TicketViewSet, basename='tickets')
 router.register("events", EventViewSet, basename='events')
-router.register("offline-lectures", OfflineLectureViewset, basename='offline-lectures')
+router.register("offline-lectures",
+                OfflineLectureViewset,
+                basename='offline-lectures')
+router.register("lecture-categories",
+                LectureCategoriesViewSet,
+                basename='lecture-categories')
 router.register("lecturers", LecturersViewset, basename='lecturers')
 
 urlpatterns = [
-    path("payments/", view=PaymentCreateView.as_view(), name="payment-create-view"),
-    path("checkout-wh/", YandexWebhook.as_view(), name="yandex-checkout-webhook"),
-    path("cloudpayments-wh/", CloudpaymentsWebhook.as_view(), name="cloudpayments-webhook"),
- ]
+    path("payments/",
+         view=PaymentCreateView.as_view(),
+         name="payment-create-view"),
+    path("checkout-wh/",
+         YandexWebhook.as_view(),
+         name="yandex-checkout-webhook"),
+    path("cloudpayments-wh/",
+         CloudpaymentsWebhook.as_view(),
+         name="cloudpayments-webhook"),
+]
 
 app_name = "api"
 urlpatterns += router.urls
