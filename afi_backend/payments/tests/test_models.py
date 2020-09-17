d575d22af638283931ac27b9440a2f6fc071ac7b
dkol <denis.kolosov@gmail.com>
2020-09-17 14:57:13 +0300

    Fix tests

--- afi_backend/payments/tests/test_models.py
+++ afi_backend/payments/tests/test_models.py
@@ -1,9 +1,10 @@
 import pytest
 
-from afi_backend.payments.models import Payment, PaymentMethod, create_payment_with_paid_object
+from afi_backend.payments.models import Payment, PaymentMethod, create_payment_with_paid_object_and_link
 from afi_backend.tickets.models import Ticket
 from afi_backend.payments.tests.factories import PaymentMethodFactory
 from afi_backend.users.tests.factories import UserFactory
+from afi_backend.events.tests.factories import OfflineLectureFactory
 
 pytestmark = pytest.mark.django_db
 
@@ -11,9 +12,13 @@ pytestmark = pytest.mark.django_db
 def test_create_payment_func():
     payment_method = PaymentMethodFactory()
     test_user = UserFactory()
-    test_payment = create_payment_with_paid_object(
+    offline_lecture = OfflineLectureFactory()
+    test_payment = create_payment_with_paid_object_and_link(
         payment_type=payment_method.payment_type,
         user=test_user,
-        payment_for='ticket')
+        payment_for='ticket',
+        related_object_id=offline_lecture.id
+    )
     assert test_payment == Payment.objects.first()
     assert test_payment.content_object == Ticket.objects.first()
+    assert test_payment.content_object.offline_lecture == offline_lecture
