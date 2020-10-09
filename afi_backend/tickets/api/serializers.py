741046baa70dbebd50bffc949e658e94728c2ac7
Denis Kolosov <denis.kolosov@gmail.com>
2020-10-09 10:26:05 +0300

    Use user email and id for ticket creation

--- afi_backend/tickets/api/serializers.py
+++ afi_backend/tickets/api/serializers.py
@@ -5,6 +5,8 @@ from rest_framework_json_api import serializers
 from afi_backend.events.api.serializers import EventTypeSerializer
 from afi_backend.tickets.models import QRCode, Ticket
 from afi_backend.events.api.serializers import OfflineLectureSerializer
+from afi_backend.users.models import User
+from afi_backend.events.models import OfflineLecture
 
 
 class TicketSerializer(serializers.ModelSerializer):
@@ -13,20 +15,31 @@ class TicketSerializer(serializers.ModelSerializer):
     included_serializers = {
         'offline_lecture': OfflineLectureSerializer,
     }
+    customer_email = serializers.EmailField(source='customer.email')
+    offline_lecture_id = serializers.IntegerField(source='offline_lecture.id')
 
     class Meta:
         model = Ticket
         fields = [
-            'customer',
+            'customer_email',
             'activation_link',
             'scanned',
-            'offline_lecture',
+            'offline_lecture_id',
         ]
 
     def get_activation_link(self, obj):
         if obj.get_qr_code():
             return f"{settings.SITE_URL}/api/tickets/activate/{obj.qrcode.code}"
         return ""
+
+    def create(self, validated_data):
+        customer = validated_data.get('customer')
+        offline_lecture = validated_data.get('offline_lecture')
+        customer = User.objects.get(email=customer.get('email'))
+        offline_lecture = OfflineLecture.objects.get(
+            id=offline_lecture.get('id'))
+        return Ticket.objects.create(customer=customer,
+                                     offline_lecture=offline_lecture)
 
 
 class QRCodeSerializer(serializers.ModelSerializer):
