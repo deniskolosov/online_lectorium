767ac2fc32d9aac8b097472bb3c1d7e4335cac66
dkol <denis.kolosov@gmail.com>
2020-11-11 16:13:05 +0300

    Remove session middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -167,7 +167,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
+    # "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
