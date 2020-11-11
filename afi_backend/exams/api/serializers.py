e0c769bc68e82b3c96c2a899e588859b9421c798
dkol <denis.kolosov@gmail.com>
2020-11-11 20:05:02 +0300

    Add id to exam serializer

--- afi_backend/exams/api/serializers.py
+++ afi_backend/exams/api/serializers.py
@@ -32,6 +32,7 @@ class ExamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exam
         fields = [
+            'id',
             'test_assignment_id',
             'user',
             'results',
